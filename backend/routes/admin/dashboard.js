894844f6616d6ca46394aa8ca7f947086ecb0789
Filip-Janjesic <filip.janjesic@gmail.com>
2024-04-14 21:45:47 +0200

    Create dashboard.js

---
+++ backend/routes/admin/dashboard.js
@@ -0,0 +1,10 @@
+
+module.exports = async (request, reply) => {
+  try {
+    return { success: true, message: 'Admin dashboard' };
+
+  } catch (err) {
+    console.log(err);
+    return { success: false, error: 'ERR-/user/dashboard::' + err.message };
+  }
+};
