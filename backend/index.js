dfa169c431fa66c8a77975ae43983afcaadeb1ba
Filip-Janjesic <filip.janjesic@gmail.com>
2024-04-14 21:39:05 +0200

    Create index.js

---
+++ backend/index.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+require('reflect-metadata');
+const fastify = require('fastify');
+const cors = require('@fastify/cors');
+const { createConnection } = require('typeorm');
+const app = fastify({ logger: false });
+
+
+/** start the API HTTP server */
+const start = async () => {
+  try {
+    global.connection = await createConnection();
+
+    await app.listen({ port: 3000 });
+    app.log.info(`Server listening on ${app.server.address().port}`);
+  } catch (err) {
+    console.log(err);
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+start();
+
+
+/** CORS */
+app.register(cors, {
+  origin: '*', // Change this to your desired origin
+  methods: 'GET,POST,PUT,DELETE', // Adjust allowed methods as needed
+});
+
+
+/** routes */
+const verifyToken = require('./routes/handlers/verifyToken.js');
+const hasRoles = require('./routes/handlers/hasRoles.js');
+
+
+const usersCommon = require('./routes/usersCommon.js');
+app.post('/users/register', { schema: { body: usersCommon.usersRegisterSchema } }, usersCommon.register);
+app.post('/users/login', { schema: { body: usersCommon.usersLoginSchema } }, usersCommon.login);
+
+// user routes
+const dashboardU = require('./routes/user/dashboard.js');
+app.get('/user/dashboard', { preHandler: [verifyToken, hasRoles.userRole] }, dashboardU);
+
+// admin routes
+const dashboardA = require('./routes/admin/dashboard.js');
+app.get('/admin/dashboard', { preHandler: [verifyToken, hasRoles.adminRole] }, dashboardA);
